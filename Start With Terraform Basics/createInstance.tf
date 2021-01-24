8e19b36489578e7563c15485e0481ebd9baf6e74
“Eric <eands9@yahoo.com>
2021-01-24 07:50:16 -0600

    hg

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,5 +1,10 @@
 
 resource "aws_instance" "MyFirstInstance" {
+    count=3
     ami = "ami-04e9aec1ab665f323"
     instance_type = "t2.micro"
+
+    tags{
+        Name="threedemoinstance"
+    }
 }
