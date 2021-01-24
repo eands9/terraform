7518d46778853dd03f5ceccb5fbb27304eeb2342
“Eric <eands9@yahoo.com>
2021-01-23 21:30:06 -0600

    2

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key = "AKIASFB2T2TYZ64QI65B"
-    secret_key = "Secret_Key"
-    region = "us-west-2"
-}
 
 resource "aws_instance" "MyFirstInstance" {
     ami = "ami-04e9aec1ab665f323"
---
+++ Start With Terraform Basics/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIASFB2T2TYZ64QI65B"
+    secret_key = "Secret_Key"
+    region = "us-west-2"
+}
