e91853465ca942c5331566d652b8893c504b1b08
“Eric <eands9@yahoo.com>
2021-01-24 20:53:21 -0600

    k

---
+++ Start With Terraform Basics/Terraform_Concepts_Building_Blocks/Demo_DataSource/variables.tf
@@ -0,0 +1,21 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default="us-west-2"
+}
+
+variable "Security_Group"{
+    type=list
+    default=["sg-24076","sg-0001","sg-0002"]
+}
+
+variable "AMIS"{
+    type=map
+    default={
+        us-east-1 = "ami-0f593aebffc0070e1"
+        us-east-2 = "ami-096cdd827d38ca4d8"
+        us-west-1 = "ami-0a87a7625a4f44ffb"
+        us-west-2 = "ami-04e9aec1ab665f323"
+    }
+}
+
